3b9705a32adc135bf98e482ea5c251567180baea
youncccat <1306036576@qq.com>
2019-11-27 23:20:09 +0800

    [Refactor] struct of directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './components/app/app-routing.module';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
