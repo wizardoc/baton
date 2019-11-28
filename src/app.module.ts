235d4f900d5aa499e1ce96960a4d0bad58e7e130
youncccat <1306036576@qq.com>
2019-11-28 23:11:56 +0800

    [Feat] import some module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,41 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app/app-routing.module";
 import { AppComponent } from "./app/app.component";
 import { HeaderBarComponent } from "./app/header-bar/header-bar.component";
-import { MatToolbarModule } from "@angular/material";
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatRippleModule,
+  MatButtonModule,
+  MatSnackBarModule
+} from "@angular/material";
+import { LoginCardComponent } from "./app/login-card/login-card.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpInfo } from "./services";
+import { UserService } from "./services/user-service";
 
 @NgModule({
-  declarations: [AppComponent, HeaderBarComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
-  providers: [],
+  declarations: [AppComponent, HeaderBarComponent, LoginCardComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatRippleModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule
+  ],
+  providers: [HttpInfo, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
