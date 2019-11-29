8c37019a9f784c2816a1b305783d673de2d35197
youncccat <1306036576@qq.com>
2019-11-29 10:55:22 +0800

    [Feat] preload main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 import { PanelComponent } from "src/panel/panel.component";
 import { AppComponent } from "./app.component";
 import { EntryComponent } from "./entry/entry.component";
@@ -8,11 +8,19 @@ import { MainCanActiveGuard } from "../guards/main/active";
 const routes: Routes = [
   { path: "", redirectTo: "/entry", pathMatch: "full" },
   { path: "entry", component: EntryComponent },
-  { path: "main", component: PanelComponent, canActivate: [MainCanActiveGuard] }
+  {
+    path: "main",
+    component: PanelComponent,
+    canActivate: [MainCanActiveGuard]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
